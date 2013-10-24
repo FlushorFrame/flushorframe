df2a114369c131494da4c5ae3452d5d72c9cdd37
Flush or Frame <flushorframe@gmail.com>
2013-10-23 20:37:03 -0400

    Initial Code Load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlushorFrame::Application.config.secret_token = '5e30fb56538fa7ed5e7ff0f8da2911490e4cd7a75efd8adf7e5fbad4b2d916b755eaf6fd5ca339addeffed91f4a07ede8cbbd98d43e2fa0bdeae83b619b9bdf3'
